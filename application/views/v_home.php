66f5967a87b9ec0c5e5a6a72ef0836084409e83b
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-04-08 19:54:52 +0700

    added simple gis

---
+++ application/views/v_home.php
@@ -0,0 +1,115 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SISTEM INFORMASI GEOGRAFIS KOTA PATI</title>
+<link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+  <!-- Bootstrap -->
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+  <link href="<?=base_url()?>assets/css/BootSideMenu.css" rel="stylesheet">
+  <link href="<?=base_url()?>assets/leaflet/leaflet.css" rel="stylesheet">
+
+<style type="text/css">
+  .user{
+    padding:5px;
+    margin-bottom: 5px;
+  }
+  #mapid { height: 480px; }
+</style>
+</head>
+<body>
+
+  <!--Test -->
+  <div id="test">
+    <h2>MENU</h2>
+    <div class="list-group">
+      <a href="#" class="list-group-item active">Cras justo odio</a>
+      <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+      <a href="#" class="list-group-item">Morbi leo risus</a>
+      <a href="#" class="list-group-item">Morbi leo risus</a>
+      <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+      <a href="#subTest" class="list-group-item">Sub menù test</a>
+      <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+      <a href="#" class="list-group-item">Morbi leo risus</a>
+      <a href="#" class="list-group-item">Porta Porta Porta sdaf s fs hfuis uif ac consectetur ac</a>
+    </div>
+
+  </div>
+  <!--/Test -->
+
+  <!--Normale contenuto di pagina-->
+  <div class="container">
+    
+
+    <div class="row">
+      <div class="col-md-12">
+          <h1>GIS PATI</h1>
+		  <div id="carbon-block"></div>
+<script>
+try {
+  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+    return true;
+  }).catch(function(e) {
+    var carbonScript = document.createElement("script");
+    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+    carbonScript.id = "_carbonads_js";
+    document.getElementById("carbon-block").appendChild(carbonScript);
+  });
+} catch (error) {
+  console.log(error);
+}
+</script>
+        </div>
+    </div>
+
+	<div class="row">
+      <div class="col-md-12">
+	  <div id="mapid"></div>  
+		  <div id="carbon-block"></div>
+<script>
+try {
+  fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", { method: 'HEAD', mode: 'no-cors' })).then(function(response) {
+    return true;
+  }).catch(function(e) {
+    var carbonScript = document.createElement("script");
+    carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CK7DKKQU&placement=wwwjqueryscriptnet";
+    carbonScript.id = "_carbonads_js";
+    document.getElementById("carbon-block").appendChild(carbonScript);
+  });
+} catch (error) {
+  console.log(error);
+}
+</script>
+        </div>
+    </div>
+
+	
+</div>
+  <!--Normale contenuto di pagina-->
+
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="<?=base_url()?>assets/js/BootSideMenu.js"></script>
+  <script src="<?=base_url()?>assets/leaflet/leaflet.js"></script>
+
+  <script type="text/javascript">
+    $('#test').BootSideMenu({side:"left", autoClose:false});
+	var map = L.map('mapid').setView([-6.7535, 111.0399], 13);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	L.marker([-6.7535, 111.0399]).addTo(map)
+		.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+		.openPopup();
+  </script>
+</body>
+</html>
